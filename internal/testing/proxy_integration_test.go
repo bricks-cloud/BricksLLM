a37ac93c398ba263c2acfb1cb0de719e08ffddf6
Spike Lu <luyuanxin1995@gmail.com>
2023-08-29 11:59:54 -0700

    add integration test for ttl and cost limit

--- internal/testing/proxy_integration_test.go
+++ internal/testing/proxy_integration_test.go
@@ -3,6 +3,7 @@ package testing
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"io"
 	"net/http"
 	"net/url"
@@ -14,6 +15,37 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
+
+func proxyRequest(request *openai.ChatCompletionRequest, apiKey string) (int, []byte, error) {
+	jsonData, err := json.Marshal(request)
+
+	if err != nil {
+		return 0, nil, err
+	}
+
+	resp, err := http.DefaultClient.Do(&http.Request{
+		Method: http.MethodPost,
+		URL:    &url.URL{Scheme: "http", Host: "localhost:8002", Path: "/api/providers/openai/v1/chat/completions"},
+		Header: map[string][]string{
+			"Content-Type":  {"application/json"},
+			"Authorization": {fmt.Sprintf("Bearer %s", apiKey)},
+		},
+		Body: io.NopCloser(bytes.NewBuffer(jsonData)),
+	})
+
+	if err != nil {
+		return 0, nil, err
+	}
+
+	defer resp.Body.Close()
+
+	bs, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return 0, nil, err
+	}
+
+	return resp.StatusCode, bs, err
+}
 
 func TestProxy_AccessControl(t *testing.T) {
 	t.Run("when request to proxy does not have an api key", func(t *testing.T) {
@@ -27,22 +59,9 @@ func TestProxy_AccessControl(t *testing.T) {
 				},
 			},
 		}
-		jsonData, err := json.Marshal(request)
-		if err != nil {
-			assert.Error(t, err)
-		}
-
-		resp, err := http.DefaultClient.Do(&http.Request{
-			Method: http.MethodPost,
-			URL:    &url.URL{Scheme: "http", Host: "localhost:8002", Path: "/api/providers/openai/v1/chat/completions"},
-			Header: map[string][]string{
-				"Content-Type": {"application/json"},
-			},
-			Body: io.NopCloser(bytes.NewBuffer(jsonData)),
-		})
-
+		code, _, err := proxyRequest(request, "")
 		require.Nil(t, err)
-		assert.Equal(t, http.StatusUnauthorized, resp.StatusCode)
+		assert.Equal(t, http.StatusUnauthorized, code)
 	})
 
 	t.Run("when api key is valid", func(t *testing.T) {
@@ -56,7 +75,7 @@ func TestProxy_AccessControl(t *testing.T) {
 		_, err := setUpApiKey(requestKey)
 		require.Nil(t, err)
 
-		time.Sleep(11 * time.Second)
+		time.Sleep(2 * time.Second)
 
 		request := &openai.ChatCompletionRequest{
 			Model: "gpt-4",
@@ -67,26 +86,76 @@ func TestProxy_AccessControl(t *testing.T) {
 				},
 			},
 		}
-		jsonData, err := json.Marshal(request)
+
+		code, bs, err := proxyRequest(request, requestKey.Key)
+		require.Nil(t, err)
+		assert.Equal(t, http.StatusOK, code, string(bs))
+	})
+
+	t.Run("when api key has a ttl", func(t *testing.T) {
+		t.Skip()
+		requestKey := &key.RequestKey{
+			Name: "Spike's Testing Key",
+			Tags: []string{"spike"},
+			Key:  "actualKey",
+			Ttl:  "5s",
+		}
+
+		_, err := setUpApiKey(requestKey)
 		require.Nil(t, err)
 
-		resp, err := http.DefaultClient.Do(&http.Request{
-			Method: http.MethodPost,
-			URL:    &url.URL{Scheme: "http", Host: "localhost:8002", Path: "/api/providers/openai/v1/chat/completions"},
-			Header: map[string][]string{
-				"Content-Type":  {"application/json"},
-				"Authorization": {"Bearer actualKey"},
+		time.Sleep(2 * time.Second)
+		request := &openai.ChatCompletionRequest{
+			Model: "gpt-4",
+			Messages: []openai.RequestMessage{
+				{
+					Role:    "system",
+					Content: "hi",
+				},
 			},
-			Body: io.NopCloser(bytes.NewBuffer(jsonData)),
-		})
+		}
+		code, bs, err := proxyRequest(request, requestKey.Key)
+		require.Nil(t, err)
+		assert.Equal(t, http.StatusOK, code, string(bs))
 
+		time.Sleep(3 * time.Second)
+
+		code, bs, err = proxyRequest(request, requestKey.Key)
+		require.Nil(t, err)
+		assert.Equal(t, http.StatusUnauthorized, code, string(bs))
+	})
+
+	t.Run("when api key has a total spend limit", func(t *testing.T) {
+		requestKey := &key.RequestKey{
+			Name:           "Spike's Testing Key",
+			Tags:           []string{"spike"},
+			Key:            "actualKey",
+			CostLimitInUsd: 0.00007,
+		}
+
+		_, err := setUpApiKey(requestKey)
 		require.Nil(t, err)
 
-		defer resp.Body.Close()
-
-		bs, err := io.ReadAll(resp.Body)
+		time.Sleep(2 * time.Second)
+		request := &openai.ChatCompletionRequest{
+			Model: "gpt-3.5-turbo",
+			Messages: []openai.RequestMessage{
+				{
+					Role:    "system",
+					Content: "hi",
+				},
+			},
+		}
+		code, bs, err := proxyRequest(request, requestKey.Key)
 		require.Nil(t, err)
+		assert.Equal(t, http.StatusOK, code, string(bs))
 
-		assert.Equal(t, http.StatusOK, resp.StatusCode, string(bs))
+		code, bs, err = proxyRequest(request, requestKey.Key)
+		require.Nil(t, err)
+		assert.Equal(t, http.StatusOK, code, string(bs))
+
+		code, bs, err = proxyRequest(request, requestKey.Key)
+		require.Nil(t, err)
+		assert.Equal(t, http.StatusUnauthorized, code, string(bs))
 	})
 }
