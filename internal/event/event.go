86063ba9f8de8ce8b2b763b61897e220a6d6d0c2
Spike Lu <luyuanxin1995@gmail.com>
2023-11-22 11:16:52 -0800

    add custom id to event

--- internal/event/event.go
+++ internal/event/event.go
@@ -3,17 +3,18 @@ package event
 import "github.com/bricks-cloud/bricksllm/internal/provider"
 
 type Event struct {
-	Id                   string
-	CreatedAt            int64
-	Tags                 []string
-	KeyId                string
-	CostInUsd            float64
-	Provider             provider.Provider
-	Model                string
-	Status               int
-	PromptTokenCount     int
-	CompletionTokenCount int
-	LatencyInMs          int
-	Path                 string
-	Method               string
+	Id                   string            `json:"id"`
+	CreatedAt            int64             `json:"created_at"`
+	Tags                 []string          `json:"tags"`
+	KeyId                string            `json:"key_id"`
+	CostInUsd            float64           `json:"cost_in_usd"`
+	Provider             provider.Provider `json:"provider"`
+	Model                string            `json:"model"`
+	Status               int               `json:"status"`
+	PromptTokenCount     int               `json:"prompt_token_count"`
+	CompletionTokenCount int               `json:"completion_token_count"`
+	LatencyInMs          int               `json:"latency_in_ms"`
+	Path                 string            `json:"path"`
+	Method               string            `json:"method"`
+	CustomId             string            `json:"custom_id"`
 }
