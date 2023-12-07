d7a91c1f694579cd1979e32136d2f156b4810d6c
Spike Lu <luyuanxin1995@gmail.com>
2023-12-07 11:47:06 -0800

    add allowedModels field

--- internal/provider/setting.go
+++ internal/provider/setting.go
@@ -1,10 +1,18 @@
 package provider
 
 type Setting struct {
-	CreatedAt int64             `json:"createdAt"`
-	UpdatedAt int64             `json:"updatedAt"`
-	Provider  string            `json:"provider"`
-	Setting   map[string]string `json:"setting,omitempty"`
-	Id        string            `json:"id"`
-	Name      string            `json:"name"`
+	CreatedAt     int64             `json:"createdAt"`
+	UpdatedAt     int64             `json:"updatedAt"`
+	Provider      string            `json:"provider"`
+	Setting       map[string]string `json:"setting,omitempty"`
+	Id            string            `json:"id"`
+	Name          string            `json:"name"`
+	AllowedModels []string          `json:"allowedModels"`
+}
+
+type UpdateSetting struct {
+	UpdatedAt     int64             `json:"updatedAt"`
+	Setting       map[string]string `json:"setting,omitempty"`
+	Name          *string           `json:"name"`
+	AllowedModels *[]string         `json:"allowedModels,omitempty"`
 }
