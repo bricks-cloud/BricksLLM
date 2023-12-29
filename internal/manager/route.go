4d69df2340b308e2b814d051b00603b39bc7ee29
Spike Lu <luyuanxin1995@gmail.com>
2023-12-28 23:08:54 -0800

    add validation for checking model congruency

--- internal/manager/route.go
+++ internal/manager/route.go
@@ -1,6 +1,7 @@
 package manager
 
 import (
+	"errors"
 	"fmt"
 	"strings"
 	"time"
@@ -25,13 +26,15 @@ type RouteManager struct {
 	s  RoutesStorage
 	ks Storage
 	ms RoutesMemStorage
+	ps ProviderSettingsMemStorage
 }
 
-func NewRouteManager(s RoutesStorage, ks Storage, ms RoutesMemStorage) *RouteManager {
+func NewRouteManager(s RoutesStorage, ks Storage, ms RoutesMemStorage, psm ProviderSettingsMemStorage) *RouteManager {
 	return &RouteManager{
 		s:  s,
 		ks: ks,
 		ms: ms,
+		ps: psm,
 	}
 }
 
@@ -59,6 +62,130 @@ func (m *RouteManager) CreateRoute(r *route.Route) (*route.Route, error) {
 	return m.s.CreateRoute(r)
 }
 
+func checkModelValidity(provider, model string) bool {
+	if provider == "azure" {
+		return contains(model, azureSupportedModels)
+	}
+
+	if provider == "openai" {
+		return contains(model, openaiSupportedModels)
+	}
+
+	return false
+}
+
+var (
+	azureSupportedModels = []string{
+		"gpt-4-1106-preview",
+		"gpt-4-1106-vision-preview",
+		"gpt-4",
+		"gpt-4-0314",
+		"gpt-4-0613",
+		"gpt-4-32k",
+		"gpt-4-32k-0613",
+		"gpt-4-32k-0314",
+		"gpt-35-turbo",
+		"gpt-35-turbo-1106",
+		"gpt-35-turbo-0301",
+		"gpt-35-turbo-instruct",
+		"gpt-35-turbo-0613",
+		"gpt-35-turbo-16k",
+		"gpt-35-turbo-16k-0613",
+		"ada",
+	}
+
+	openaiSupportedModels = []string{
+		"gpt-4-1106-preview",
+		"gpt-4-1106-vision-preview",
+		"gpt-4",
+		"gpt-4-0314",
+		"gpt-4-0613",
+		"gpt-4-32k",
+		"gpt-4-32k-0613",
+		"gpt-4-32k-0314",
+		"gpt-3.5-turbo",
+		"gpt-3.5-turbo-1106",
+		"gpt-3.5-turbo-0301",
+		"gpt-3.5-turbo-instruct",
+		"gpt-3.5-turbo-0613",
+		"gpt-3.5-turbo-16k",
+		"gpt-3.5-turbo-16k-0613",
+		"text-embedding-ada-002",
+	}
+
+	supportedModels = []string{
+		"gpt-4-1106-preview",
+		"gpt-4-1106-vision-preview",
+		"gpt-4",
+		"gpt-4-0314",
+		"gpt-4-0613",
+		"gpt-4-32k",
+		"gpt-4-32k-0613",
+		"gpt-4-32k-0314",
+		"gpt-35-turbo",
+		"gpt-35-turbo-1106",
+		"gpt-35-turbo-0301",
+		"gpt-35-turbo-instruct",
+		"gpt-35-turbo-0613",
+		"gpt-35-turbo-16k",
+		"gpt-35-turbo-16k-0613",
+		"gpt-3.5-turbo",
+		"gpt-3.5-turbo-1106",
+		"gpt-3.5-turbo-0301",
+		"gpt-3.5-turbo-instruct",
+		"gpt-3.5-turbo-0613",
+		"gpt-3.5-turbo-16k",
+		"gpt-3.5-turbo-16k-0613",
+		"ada",
+		"text-embedding-ada-002",
+	}
+
+	adaModels = []string{
+		"ada",
+		"text-embedding-ada-002",
+	}
+
+	chatCompletionModels = []string{
+		"gpt-35-turbo",
+		"gpt-35-turbo-1106",
+		"gpt-35-turbo-0301",
+		"gpt-35-turbo-instruct",
+		"gpt-35-turbo-0613",
+		"gpt-35-turbo-16k",
+		"gpt-35-turbo-16k-0613",
+		"gpt-4-1106-preview",
+		"gpt-4-1106-vision-preview",
+		"gpt-4",
+		"gpt-4-0314",
+		"gpt-4-0613",
+		"gpt-4-32k",
+		"gpt-4-32k-0613",
+		"gpt-4-32k-0314",
+		"gpt-3.5-turbo",
+		"gpt-3.5-turbo-1106",
+		"gpt-3.5-turbo-0301",
+		"gpt-3.5-turbo-instruct",
+		"gpt-3.5-turbo-0613",
+		"gpt-3.5-turbo-16k",
+		"gpt-3.5-turbo-16k-0613",
+	}
+
+	supportedProviders = []string{
+		"openai",
+		"azure",
+	}
+)
+
+func contains(target string, source []string) bool {
+	for _, s := range source {
+		if s == target {
+			return true
+		}
+	}
+
+	return false
+}
+
 func (m *RouteManager) validateRoute(r *route.Route) error {
 	fields := []string{}
 
@@ -78,12 +205,18 @@ func (m *RouteManager) validateRoute(r *route.Route) error {
 		fields = append(fields, "steps")
 	}
 
+	containAda := false
+
 	for index, step := range r.Steps {
 		if len(step.Provider) == 0 {
 			fields = append(fields, fmt.Sprintf("steps.[%d].provider", index))
 		}
 
-		if step.Provider == "azure" && len(step.Params) == 0 {
+		if !contains(step.Provider, supportedProviders) {
+			return errors.New(fmt.Sprintf("steps.[%d].provider is not supported. Only azure and openai are supported", index))
+		}
+
+		if step.Provider == "azure" {
 			apiVersion, _ := step.Params["apiVersion"]
 			if len(apiVersion) == 0 {
 				fields = append(fields, fmt.Sprintf("steps.[%d].params.apiVersion", index))
@@ -93,12 +226,32 @@ func (m *RouteManager) validateRoute(r *route.Route) error {
 			if len(deploymentId) == 0 {
 				fields = append(fields, fmt.Sprintf("steps.[%d].params.deploymentId", index))
 			}
-
-			fields = append(fields, fmt.Sprintf("steps.[%d].provider", index))
 		}
 
 		if len(step.Model) == 0 {
 			fields = append(fields, fmt.Sprintf("steps.[%d].model", index))
+		}
+
+		if !contains(step.Model, supportedModels) {
+			return errors.New(fmt.Sprintf("steps.[%d].model is not supported. Only chat completion and embeddings model are supported.", index))
+		}
+
+		if !checkModelValidity(step.Provider, step.Model) {
+			return errors.New(fmt.Sprintf("model: %s is not supported for provider: %s.", step.Model, step.Provider))
+		}
+
+		if !containAda && contains(step.Model, adaModels) {
+			containAda = true
+		}
+	}
+
+	for _, step := range r.Steps {
+		if containAda && !contains(step.Model, adaModels) {
+			return errors.New("steps must have congruent models. Chat completion and embedding models cannot be in the same route config.")
+		}
+
+		if !containAda && !contains(step.Model, chatCompletionModels) {
+			return errors.New("steps must have congruent models. Chat completion and embedding models cannot be in the same route config.")
 		}
 	}
 
@@ -117,8 +270,13 @@ func (m *RouteManager) validateRoute(r *route.Route) error {
 		return err
 	}
 
-	if len(found) != len(r.KeyIds) {
-		return internal_errors.NewValidationError("specified key ids are not found")
+	for _, key := range found {
+		settingIds := key.GetSettingIds()
+		settings := m.ps.GetSettings(settingIds)
+
+		if !r.ValidateSettings(settings) {
+			return errors.New("provider settings assosciated with the key cannot for accessing models specified in the route")
+		}
 	}
 
 	_, err = m.s.GetRouteByPath(r.Path)
