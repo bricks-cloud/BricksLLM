92021c56c4cf79d40d13173f123f127b943db595
XiangWang <wangxiang@shanda.com>
2024-05-21 21:50:08 +0800

    use koanf

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,45 +1,73 @@
 package config
 
 import (
+	"fmt"
+	"log"
 	"time"
 
-	"github.com/caarlos0/env"
+	"github.com/knadh/koanf/parsers/json"
+	"github.com/knadh/koanf/providers/env"
+	"github.com/knadh/koanf/providers/file"
+	"github.com/knadh/koanf/v2"
 )
 
 type Config struct {
-	PostgresqlHosts               string        `env:"POSTGRESQL_HOSTS" envSeparator:":" envDefault:"localhost"`
-	PostgresqlDbName              string        `env:"POSTGRESQL_DB_NAME"`
-	PostgresqlUsername            string        `env:"POSTGRESQL_USERNAME"`
-	PostgresqlPassword            string        `env:"POSTGRESQL_PASSWORD"`
-	PostgresqlSslMode             string        `env:"POSTGRESQL_SSL_MODE" envDefault:"disable"`
-	PostgresqlPort                string        `env:"POSTGRESQL_PORT" envDefault:"5432"`
-	RedisHosts                    string        `env:"REDIS_HOSTS" envSeparator:":" envDefault:"localhost"`
-	RedisPort                     string        `env:"REDIS_PORT" envDefault:"6379"`
-	RedisUsername                 string        `env:"REDIS_USERNAME"`
-	RedisPassword                 string        `env:"REDIS_PASSWORD"`
-	RedisReadTimeout              time.Duration `env:"REDIS_READ_TIME_OUT" envDefault:"1s"`
-	RedisWriteTimeout             time.Duration `env:"REDIS_WRITE_TIME_OUT" envDefault:"500ms"`
-	PostgresqlReadTimeout         time.Duration `env:"POSTGRESQL_READ_TIME_OUT" envDefault:"2m"`
-	PostgresqlWriteTimeout        time.Duration `env:"POSTGRESQL_WRITE_TIME_OUT" envDefault:"5s"`
-	InMemoryDbUpdateInterval      time.Duration `env:"IN_MEMORY_DB_UPDATE_INTERVAL" envDefault:"5s"`
-	OpenAiKey                     string        `env:"OPENAI_API_KEY"`
-	StatsProvider                 string        `env:"STATS_PROVIDER"`
-	AdminPass                     string        `env:"ADMIN_PASS"`
-	ProxyTimeout                  time.Duration `env:"PROXY_TIMEOUT" envDefault:"600s"`
-	NumberOfEventMessageConsumers int           `env:"NUMBER_OF_EVENT_MESSAGE_CONSUMERS" envDefault:"3"`
-	OpenAiApiKey                  string        `env:"OPENAI_API_KEY"`
-	CustomPolicyDetectionTimeout  time.Duration `env:"CUSTOM_POLICY_DETECTION_TIMEOUT" envDefault:"10m"`
-	AmazonRegion                  string        `env:"AMAZON_REGION" envDefault:"us-west-2"`
-	AmazonRequestTimeout          time.Duration `env:"AMAZON_REQUEST_TIMEOUT" envDefault:"5s"`
-	AmazonConnectionTimeout       time.Duration `env:"AMAZON_CONNECTION_TIMEOUT" envDefault:"10s"`
+	PostgresqlHosts               string
+	PostgresqlDbName              string
+	PostgresqlUsername            string
+	PostgresqlPassword            string
+	PostgresqlSslMode             string
+	PostgresqlPort                string
+	RedisHosts                    string
+	RedisPort                     string
+	RedisUsername                 string
+	RedisPassword                 string
+	RedisReadTimeout              time.Duration
+	RedisWriteTimeout             time.Duration
+	PostgresqlReadTimeout         time.Duration
+	PostgresqlWriteTimeout        time.Duration
+	InMemoryDbUpdateInterval      time.Duration
+	OpenAiKey                     string
+	StatsProvider                 string
+	AdminPass                     string
+	ProxyTimeout                  time.Duration
+	NumberOfEventMessageConsumers int
+	OpenAiApiKey                  string
+	CustomPolicyDetectionTimeout  time.Duration
+	AmazonRegion                  string
+	AmazonRequestTimeout          time.Duration
+	AmazonConnectionTimeout       time.Duration
 }
 
+var k = koanf.New(".")
+
 func ParseEnvVariables() (*Config, error) {
-	cfg := &Config{}
-	err := env.Parse(cfg)
-	if err != nil {
-		return nil, err
+
+	f := file.Provider("config.json")
+	if err := k.Load(f, json.Parser()); err != nil {
+		log.Fatalf("error loading config: %v", err)
 	}
+	k.Load(env.Provider("", ".", func(s string) string {
+		return s
+	}), nil)
+
+	f.Watch(func(event interface{}, err error) {
+		if err != nil {
+			log.Printf("watch error: %v", err)
+			return
+		}
+
+		// Throw away the old config and load a fresh copy.
+		log.Println("config changed. Reloading ...")
+		k = koanf.New(".")
+		k.Load(f, json.Parser())
+		k.Print()
+	})
+
+	fmt.Println("name is = ", k.Duration("RedisWriteTimeout").String())
+
+	cfg := &Config{}
+	k.Unmarshal("", cfg)
 
 	return cfg, nil
 }
